882f8cc6a280bdbd5dba9ee2af53bbb316662c97
Miccio <minicornetto@outlook.com>
2020-08-25 23:57:49 +0800

    2020-08-25

---
+++ leetcode.dotnet/leetcode.dotnet/0459_Repeated Substring Pattern.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode.dotnet
+{
+    class _0459_Repeated_Substring_Pattern
+    {
+public class Solution
+{
+    public bool RepeatedSubstringPattern(string s)
+    {
+        for (var len = 1; len <= s.Length / 2; len++)
+        {
+            if (s.Length % len != 0)
+            {
+                continue;
+            }
+
+            var subS = s.Substring(0, len);
+            var isRepeated = true;
+            for (var start = len; start + len <= s.Length; start += len)
+            {
+                var tmpS = s.Substring(start, len);
+                if (tmpS != subS)
+                {
+                    isRepeated = false;
+                    break;
+                }
+            }
+
+            if (isRepeated)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+    }
+}
---
+++ leetcode.dotnet/leetcode.dotnet/0491_Increasing Subsequences.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace leetcode.dotnet
+{
+    class _0491_Increasing_Subsequences
+    {
+public class Solution
+{
+    public IList<IList<int>> FindSubsequences(int[] nums)
+    {
+        IList<IList<int>> retNums = new List<IList<int>>();
+        if (nums == null || nums.Length < 2)
+        {
+            return retNums;
+        }
+
+        var set = new HashSet<string>();
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            FindSubsequences(nums, i + 1, new List<int> { nums[i] }, retNums, set );
+        }
+
+        return retNums;
+    }
+
+    public void FindSubsequences(int[] nums, int start, IList<int> subNums, IList<IList<int>> retNums, HashSet<string> set)
+    {
+        if (start == nums.Length)
+        {
+            return;
+        }
+
+        for (var i = start; i < nums.Length; i++)
+        {
+            if (nums[i] >= subNums.Last())
+            {
+                IList<int> curNums = new List<int>(subNums);
+                curNums.Add(nums[i]);
+                var curStr = string.Join("_", curNums);
+                if (!set.Contains(curStr))
+                {
+                    set.Add(curStr);
+                    retNums.Add(curNums);
+
+                    FindSubsequences(nums, i + 1, curNums, retNums, set);
+                }
+            }
+        }
+    }
+}
+    }
+}
