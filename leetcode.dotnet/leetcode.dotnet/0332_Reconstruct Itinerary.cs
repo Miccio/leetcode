ac4b05166d983b9762df29460ca15175910a8f2f
Miccio <minicornetto@outlook.com>
2020-08-28 09:29:48 +0800

    tmp

---
+++ leetcode.dotnet/leetcode.dotnet/0332_Reconstruct Itinerary.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.dotnet
+{
+    class _0332_Reconstruct_Itinerary
+    {
+        public class Solution
+        {
+            public IList<string> FindItinerary(IList<IList<string>> tickets)
+            {
+                var ret = new List<string>();
+                var dict = new Dictionary<string, IList<IList<string>>>();
+
+                foreach (var ticket in tickets)
+                {
+                    var src = ticket[0];
+                    if (!dict.ContainsKey(src))
+                    {
+                        dict.Add(src, new List<IList<string>>());
+                    }
+
+                    var ts = dict[src];
+                    ts.Add(ticket);
+                }
+
+                var dict2 = new Dictionary<string, IList<IList<string>>>();
+                foreach (var src in dict.Keys)
+                {
+                    dict2.Add(src, dict[src].OrderBy(t => t[1]).ToList());
+                }
+
+                dict = dict2;
+
+                var preDst = "JFK";
+                ret.Add(preDst);
+                for (var i = 1; i <= tickets.Count; i++)
+                    {
+                        var j = 0;
+                        var ts = dict[preDst];
+                        var t = ts[j++];
+                        if (i < tickets.Count)
+                        {
+                            var dst = t[1];
+                            while (!dict.ContainsKey(dst) || dict[dst].Count == 0)
+                            {
+                                t = ts[j++];
+                            }
+                        }
+
+                        preDst = t[1];
+                        ret.Add(t[1]);
+                        ts.Remove(t);
+                    }
+
+                return ret;
+            }
+        }
+    }
+}
---
+++ leetcode.dotnet/leetcode.dotnet/0657_Robot Return to Origin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode.dotnet
+{
+    class _0657_Robot_Return_to_Origin
+    {
+        public class Solution
+        {
+            public bool JudgeCircle(string moves)
+            {
+                int x = 0, y = 0;
+                foreach (var ch in moves)
+                {
+                    switch (ch)
+                    {
+                        case 'U':
+                            y++;
+                            break;
+                        case 'D':
+                            y--;
+                            break;
+                        case 'L':
+                            x--;
+                            break;
+                        case 'R':
+                            x++;
+                            break;
+                    }    
+                }
+
+                return x == 0 && y == 0;
+            }
+        }
+    }
+}
