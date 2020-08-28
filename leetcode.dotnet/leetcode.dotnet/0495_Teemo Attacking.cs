659a715dd1930dc2ebbea8dc0e7ab586345d1f9b
Miccio <minicornetto@outlook.com>
2020-08-28 23:04:34 +0800

    2020-08-28

---
+++ leetcode.dotnet/leetcode.dotnet/0495_Teemo Attacking.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode.dotnet
+{
+    public class _0495_Teemo_Attacking
+    {
+public class Solution
+{
+    public int FindPoisonedDuration(int[] timeSeries, int duration)
+    {
+        int ret = 0;
+        if (timeSeries == null || timeSeries.Length == 0)
+        {
+            return ret;
+        }
+
+        int preEnd = 0;
+        for (var i = 0; i < timeSeries.Length; i++)
+        {
+            var start = timeSeries[i];
+            var end = start + duration;
+            if (start < preEnd)
+            {
+                start = preEnd;
+            }
+
+            ret += (end - start);
+            preEnd = end;
+        }
+
+        return ret;
+    }
+}
+    }
+}
---
+++ leetcode.dotnet/leetcode.dotnet/0917_Reverse Only Letters.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode.dotnet
+{
+    class _0917_Reverse_Only_Letters
+    {
+public class Solution
+{
+    public string ReverseOnlyLetters(string S)
+    {
+        if (string.IsNullOrEmpty(S))
+        {
+            return S;
+        }
+
+        var chs = S.ToCharArray();
+        var i = 0;
+        var j = chs.Length - 1;
+        while (i < j)
+        {
+            while (i < j && !CheckCharacter(chs[i]))
+            {
+                i++;
+            }
+
+            while (i < j && !CheckCharacter(chs[j]))
+            {
+                j--;
+            }
+
+            if (i < j)
+            {
+                var tmp = chs[i];
+                chs[i] = chs[j];
+                chs[j] = tmp;
+                i++;
+                j--;
+            }
+        }
+
+        return new string(chs);
+    }
+
+    public bool CheckCharacter(char ch)
+    {
+        return (ch <= 'z' && ch >= 'a') || (ch <= 'Z' && ch >= 'A');
+    }
+}
+    }
+}
