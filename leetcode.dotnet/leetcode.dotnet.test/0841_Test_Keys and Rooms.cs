14d0e8bab06ad7cc8462c5de0755bd47257ec4e9
Qiang Hao 🎄 <qihao@microsoft.com>
2020-08-31 20:31:17 +0800

    2020-08-31

---
+++ leetcode.dotnet/leetcode.dotnet.test/0841_Test_Keys and Rooms.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode.dotnet.test
+{
+    [TestFixture]
+    class _0841_Test_Keys_and_Rooms
+    {
+        [Test]
+        public void Test()
+        {
+            var rooms = new List<IList<int>>
+            {
+                new List<int> { 1 },
+                new List<int> { },
+                new List<int> { 0, 3 },
+                new List<int> { 1 },
+            };
+
+            var sol = new _0841_Keys_and_Rooms.Solution();
+            var ret = sol.CanVisitAllRooms(rooms);
+        }
+    }
+}
---
+++ leetcode.dotnet/leetcode.dotnet/0841_Keys and Rooms.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace leetcode.dotnet
+{
+    public class _0841_Keys_and_Rooms
+    {
+        public class Solution
+        {
+            /// <summary>
+            /// Convert the key info into the two directed links.
+            /// In this logic, the directed cyclic graph is built.
+            /// Then visit the graph starting from 0, and check the visted rooms.
+            /// </summary>
+            /// <param name="rooms"></param>
+            /// <returns></returns>
+            public bool CanVisitAllRooms(IList<IList<int>> rooms)
+            {
+                if (rooms == null || rooms.Count == 0)
+                {
+                    return false;
+                }
+
+                var roomCount = rooms.Count;
+                var dict = new Dictionary<int, HashSet<int>>();
+                for (var i = 0; i < roomCount; i++)
+                {
+                    if (rooms[i] != null)
+                    {
+                        foreach (var key in rooms[i])
+                        {
+                            if (!dict.TryGetValue(i, out var ends))
+                            {
+                                ends = new HashSet<int>();
+                                dict.Add(i, ends);
+                            }
+
+                            if (!ends.Contains(key))
+                            {
+                                ends.Add(key);
+                            }
+                        }
+                    }
+                }
+
+                var visitedRooms = new HashSet<int>();
+                VisitRooms(0, dict, ref visitedRooms);
+                return visitedRooms.Count == roomCount;
+            }
+
+            public void VisitRooms(int startNum, Dictionary<int, HashSet<int>> dict, ref HashSet<int> visitedRooms)
+            {
+                if (visitedRooms.Contains(startNum))
+                {
+                    return;
+                }
+                else
+                {
+                    visitedRooms.Add(startNum);
+                }
+
+                if (!dict.TryGetValue(startNum, out var ends))
+                {
+                    return;
+                }
+
+                foreach (var end in ends)
+                {
+                    VisitRooms(end, dict, ref visitedRooms);
+                }
+            }
+        }
+    }
+}
