524e7cfd2d4399d192ea84e5bac03ae94949f38e
Qiang Hao 🎄 <qihao@microsoft.com>
2020-09-01 19:29:37 +0800

    2020-09-01

---
+++ leetcode.dotnet/leetcode.dotnet/0486_Predict the Winner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.dotnet
+{
+    class _0486_Predict_the_Winner
+    {
+public class Solution
+{
+    /// <summary>
+    /// If i == j, delta = nums[i], true
+    /// If j - i == 1, delta = Math.Abs(nums[i] - nums[j])
+    /// If j - i > 1, delta = Math.Max(nums[i] + matrix[i+1][j], nums[j] + matrix[i][j-1])
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public bool PredictTheWinner(int[] nums)
+    {
+        if (nums == null || nums.Length < 3)
+        {
+            return true;
+        }
+
+        var dp = new int[nums.Length][];
+        for (var i = nums.Length - 1; i >= 0 ; i--)
+        {
+            dp[i] = new int[nums.Length];
+            dp[i][i] = nums[i];
+
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                dp[i][j] = Math.Max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+
+        return dp[0][nums.Length - 1] >= 0;
+    }
+}
+    }
+}
---
+++ leetcode.dotnet/leetcode.dotnet/0877_Stone Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode.dotnet
+{
+    class _0877_Stone_Game
+    {
+public class Solution
+{
+    public bool StoneGame(int[] piles)
+    {
+        if (piles == null || piles.Length < 3)
+        {
+            return true;
+        }
+
+        var len = piles.Length;
+        var dp = new int[len][];
+        for (var i = 0; i < len; i++)
+        {
+            dp[i] = new int[len];
+            dp[i][i] = piles[i];
+        }
+
+        for (var i = len - 1; i >= 0; i--)
+        {
+            for (var j = i + 1; j < len; j++)
+            {
+                dp[i][j] = Math.Max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+
+        return dp[0][len - 1] >= 0;
+    }
+}
+    }
+}
