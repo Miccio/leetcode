b65c1b716b0f42bda638a82882ecb6510b9abdfe
Miccio <minicornetto@outlook.com>
2020-11-22 09:35:10 +0800

    2020-11-22

---
+++ leetcode.dotnet/leetcode.dotnet/0242_Valid Anagram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode.dotnet
+{
+    class _0242_Valid_Anagram
+    {
+        public class Solution
+        {
+            public bool IsAnagram(string s, string t)
+            {
+                if (s == null || t == null)
+                {
+                    return false;
+                }
+
+                var sa = s.ToCharArray();
+                var ta = t.ToCharArray();
+                Array.Sort(sa);
+                Array.Sort(ta);
+
+                return new string(sa) == new string(ta);
+            }
+        }
+    }
+}
