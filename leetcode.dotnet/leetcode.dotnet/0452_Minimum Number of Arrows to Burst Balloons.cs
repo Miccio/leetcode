56aee1bacdbbfb9712973992cc5683afe9ca9fa4
Miccio <minicornetto@outlook.com>
2020-11-23 19:45:44 +0800

    2020-11-23

---
+++ leetcode.dotnet/leetcode.dotnet/0452_Minimum Number of Arrows to Burst Balloons.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace leetcode.dotnet
+{
+    class _0452_Minimum_Number_of_Arrows_to_Burst_Balloons
+    {
+        public class Solution
+        {
+            public int FindMinArrowShots(int[][] points)
+            {
+                if (points == null || points.Length == 0)
+                {
+                    return 0;
+                }
+
+                Array.Sort(points, (p1, p2) => p1[1].CompareTo(p2[1]));
+                int last = points[0][1];
+                var count = 1;
+                for (var i = 1; i < points.Length; i++)
+                {
+                    if (last < points[i][0])
+                    {
+                        last = points[i][1];
+                        count++;
+                    }
+                }
+
+                return count;
+            }
+        }
+    }
+}
